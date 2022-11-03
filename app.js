84c50d3d2e624b19316928a1d7f31b3795982eaa
Rushikesh Adhav <adhavrushikesh6@gmail.com>
2022-11-03 14:46:45 +0530

    password encryption added

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 require("dotenv").config();
+require("./config/database").connect();
 const express = require("express");
+const bcrypt = require("bcryptjs");
+
+const User = require("./model/user");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello from auth system</h1>");
+});
+
+app.post("/register", async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  if (!(firstname && lastname && email && password)) {
+    res.status(400).send("All fields are required");
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    res.status(401).send("User already exists");
+  }
+
+  const myEncPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    password: myEncPassword,
+  });
 });
 
 module.exports = app;
