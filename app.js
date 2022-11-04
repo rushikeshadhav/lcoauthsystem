106abb255c07b85af10a698761a79dfeed790f1f
Rushikesh Adhav <adhavrushikesh6@gmail.com>
2022-11-04 15:36:08 +0530

    authentication done

--- app.js
+++ app.js
@@ -2,37 +2,103 @@ require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const User = require("./model/user");
+const auth = require("./middleware/auth");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello from auth system</h1>");
 });
 
 app.post("/register", async (req, res) => {
-  const { firstname, lastname, email, password } = req.body;
+  try {
+    const { firstname, lastname, email, password } = req.body;
 
-  if (!(firstname && lastname && email && password)) {
-    res.status(400).send("All fields are required");
+    if (!(firstname && lastname && email && password)) {
+      res.status(400).send("All fields are required");
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(401).send("User already exists");
+    }
+
+    const myEncPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      password: myEncPassword,
+    });
+    // Token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    user.token = token;
+
+    user.password = undefined;
+
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
   }
+});
 
-  const existingUser = await User.findOne({ email });
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  if (existingUser) {
-    res.status(401).send("User already exists");
+    if (!(email && password)) {
+      res.status(400).send("Fields are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+      user.token = token;
+      user.password = undefined;
+
+      // res.status(200).json(user);
+
+      const option = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      res.status(200).cookie("token", token, option).json({
+        success: true,
+        token,
+        user,
+      });
+    } else {
+      res.status(400).send("email or password is incorrect");
+    }
+  } catch (error) {
+    console.log(error);
   }
+});
 
-  const myEncPassword = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    firstname,
-    lastname,
-    email: email.toLowerCase(),
-    password: myEncPassword,
-  });
+app.get("/dashboard", auth, (req, res) => {
+  res.status(200).send("Secret information revealed");
 });
 
 module.exports = app;
--- model/user.js
+++ model/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     type: String,
     default: null,
   },
-  firstname: {
+  lastname: {
     type: String,
     default: null,
   },
